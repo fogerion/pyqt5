c6a48cc02919cbd025ba12a55e1298acabd7c3e8
fogerion <fogeryndbrdtl@gmail.com>
2022-02-03 15:52:08 +0900

    created text editor and ui file

---
+++ text editor/main.py
@@ -0,0 +1,68 @@
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QApplication, QFileDialog, QMainWindow, QMenuBar, QMenu
+
+import sys
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+
+        self.setWindowTitle("Редактор текста")
+        self.setGeometry(300, 250, 350, 200)
+
+
+        self.text_edit = QtWidgets.QTextEdit(self)
+        self.setCentralWidget(self.text_edit)
+
+        self.createMenuBar()
+
+
+    def createMenuBar(self):
+        self.menuBar = QMenuBar(self)
+        self.setMenuBar(self.menuBar)
+
+        fileMenu = QMenu("&Файл", self)
+        self.menuBar.addMenu(fileMenu)
+
+        fileMenu.addAction("Открыть", self.action_clicked)
+        fileMenu.addAction("Сохранить", self.action_clicked)
+
+
+    @QtCore.pyqtSlot()
+
+    def action_clicked(self):
+        action = self.sender()
+        if action.text() == "Открыть":
+            #print("Open")
+            fname = QFileDialog.getOpenFileName(self)[0]
+            try:
+                f = open(fname, 'r', encoding="utf-8")
+                with f:
+                    data = f.read()
+                    self.text_edit.setText(data)
+                f.close
+            except FileNotFoundError:
+                print("No such file")
+
+        elif action.text() == "Сохранить":
+            fname = QFileDialog.getSaveFileName(self)[0]
+            try:
+                f = open(fname, 'w', encoding="utf-8")
+                text = self.text_edit.toPlainText()
+                f.write(text)
+                f.close
+            except FileNotFoundError:
+                print("No such file")
+
+
+def application():
+    app = QApplication(sys.argv)
+    window = Window()
+
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    application()
